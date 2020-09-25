9bda969660d4285fb2dad6a912413c5d926397bb
Allen Mack <allen.mack@turner.com>
2020-09-25 16:36:12 -0400

    Installed the ShiftIt Spoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -11,31 +11,13 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
   success, obj, desc = hs.osascript.applescriptFromFile(os.getenv("HOME") .. "/.hammerspoon/clearNotifications.applescript")
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
+--[[
+-- ShiftIt Spoon - https://github.com/peterklijn/hammerspoon-shiftit
+-- ]]
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+  center = {{ 'ctrl', 'alt', 'cmd' }, 'j' },
+})
 
 --[[
 -- Dynamic reloading of the config file.
